a60bcbe49747f6833bc7c88c0a6d22f389e5aaa4
ArianZargaran <arianzf@gmail.com>
2018-05-12 09:21:34 -0700

    implementing menu component

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/Header.css";
+
+const Header = () => {
+  return (
+    <header className="iu-header">
+      <h1 className="iu-header-logo">
+        <a href="/">IU Mien</a>
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
