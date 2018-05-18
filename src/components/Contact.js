1cedb2eed06ffd85338e1cb54f915bc70e4c6313
ArianZargaran <arianzf@gmail.com>
2018-05-17 23:21:19 -0700

    just two sections needed to be markeddown

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,40 @@ import React from "react";
 const Contact = props => {
   return (
     <section id="contact" className="ium-contact">
-      <p>Contact</p>
+      <form action="/my-handling-form-page">
+        <h2>Contact</h2> 
+        <div>
+          <div>
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="user_name" />
+            <div>
+              <label for="mail">E-mail:</label>
+              <input type="email" id="mail" name="user_email" />
+            </div>
+            <div>
+              <label for="mail">Subject:</label>
+              <input type="email" id="mail" name="user_email" />
+            </div>
+          </div>
+          <div>
+            <label for="msg">Message:</label>
+            <textarea id="msg" name="user_message"></textarea>
+          </div>
+          <button>Send</button>
+        </div>
+      </form>
+      <nav>
+        <h3>Follow us on Social Media!</h3>
+        <svg width="50" height="50">
+          <circle cx="25" cy="25" r="20" fill="yellow" />
+        </svg>
+        <svg width="50" height="50">
+          <circle cx="25" cy="25" r="20" fill="yellow" />
+        </svg>
+        <svg width="50" height="50">
+          <circle cx="25" cy="25" r="20" fill="yellow" />
+        </svg>
+      </nav>
     </section>
   );
 };
